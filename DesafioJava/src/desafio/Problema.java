91a106d36731d404e4448ae00479c7953c27594f
Jose Gerardo Rincon <gerardo1995.gr32@gmail.com>
2018-10-03 10:22:45 -0500

    Se agrego la interface

---
+++ DesafioJava/src/desafio/Problema.java
@@ -0,0 +1,15 @@
+package desafio;
+import mx.com.sintelti.desafios.oporx.Solucion;
+import mx.com.sintelti.desafios.oporx.Solucion.*;
+
+public class Problema implements Solucion {
+
+    //constructor sin parametros
+public Problema(){
+    String entrada='Descoocido' ;
+}//fin del contructor
+    
+
+
+
+}
